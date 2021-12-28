be36b41cba74f121035e97d7861fb4784c8a03e6
mertakdag <akdagm128@gmail.com>
2021-12-28 19:45:51 +0300

    proje yüklendi, proje halen bitmedi dikkat!!!

---
+++ Assets/kodlar/levelbitisgecis.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelbitisgecis : MonoBehaviour
+{
+  private float delayeBeforeLoading= 3.0f;
+
+    public string scenename;
+
+    private float timelapsed;
+
+    private void Update()
+    {
+
+        timelapsed += Time.deltaTime;
+
+        if (timelapsed > delayeBeforeLoading)
+        {
+            SceneManager.LoadScene(scenename);
+
+
+        }
+    }
+
+
+
+}
